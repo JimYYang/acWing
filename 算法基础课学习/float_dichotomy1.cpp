a671dca2a9e229ca6aff5211dab8ceca05eedad8
JimYYang <99114248+JimYYang@users.noreply.github.com>
2022-07-13 23:49:14 +0800

    Update and rename float_dichotomy.cpp to float_dichotomy1.cpp

--- 算法基础课学习/float_dichotomy1.cpp
+++ 算法基础课学习/float_dichotomy1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     double x;
     cin >> x;
-    double l = 0, r = x;
+    double l = 0, r = max(1,x);//这样的二分是自己先通过判断 得到左右边界 而不同于数组二分
     while(r - l > 1e-8){
         double mid = (l + r) / 2;
         if(mid * mid > x)r = mid;
