5c40e26636619b2d296b90cb27aa001941e2b61b
JimYYang <99114248+JimYYang@users.noreply.github.com>
2022-07-13 23:24:39 +0800

    Create dichotomy2_789.cpp
    第二种二分方法 根据性质二分
    两个模板 注意给出合适的性质 然后保证更新的区间包含答案
    最后区间只有一个数 就是答案

---
+++ 算法基础课学习/dichotomy2_789.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+const int N = 100010;
+int n, k;
+int q[N];
+
+
+int main(){
+    scanf("%d%d", &n, &k);
+    for(int i = 0; i < n; ++i)scanf("%d", &q[i]);
+    
+    while(k--){
+        int tar;
+        scanf("%d", &tar);
+        
+        int l = 0, r = n - 1;
+        while(l < r){
+            int mid = (l + r) >> 1;
+            if(q[mid] >= tar)r = mid;
+            else l = mid + 1;
+        }
+        if(q[l] != tar)cout << "-1 -1" << endl;
+        else{
+            cout << l << ' ';
+            l = 0, r = n - 1;
+            while(l < r){
+                int mid = (l + r + 1) >> 1;
+                if(q[mid] <= tar)l = mid;
+                else r = mid - 1;
+            }
+            cout << r <<endl;
+        }
+    }
+    return 0;
+}
