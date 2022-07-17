7dfdf9f8d67d92f212d229e02e252d196bc4d926
JimYYang <99114248+JimYYang@users.noreply.github.com>
2022-07-17 18:42:49 +0800

    Create bit_operation_801.cpp

---
+++ 算法基础课学习/bit_operation_801.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+/* 位运算常用:
+ * 1. used |= 1 << i;  // 将第i位变成1
+ * 2. used ^= 1 << i;  // 将第i位变成0
+ * 3. n >> i & 1;      // 求第i位
+ * 4. n & -n;          // 求最右边的1
+*/
+
+const int N = 100010;
+int n;
+
+int lowbit(int x){
+    return x & -x;
+}
+int main(){
+    scanf("%d", &n);
+    while(n -- ){
+        int res = 0, x;
+        scanf("%d", &x);
+        while(x) x -= lowbit(x), res ++;
+        printf("%d ", res);
+    }
+    return 0;
+}
+
+// int main(){
+//     scanf("%d", &n);
+//     while(n--){
+//         int x, cnt = 0;
+//         scanf("%d", &x);
+//         for(int i = 31; i >= 0; i--){
+//             if(x >> i & 1)cnt++;
+//         }
+//         printf("%d ", cnt);
+//     }    
+//     return 0;
+// }
