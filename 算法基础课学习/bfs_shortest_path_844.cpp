dbceb5fc3077d7a6220bb7ea0ad27bf02498c6d7
JimYYang <99114248+JimYYang@users.noreply.github.com>
2022-07-25 21:43:57 +0800

    Update bfs_shortest_path_844.cpp

--- 算法基础课学习/bfs_shortest_path_844.cpp
+++ 算法基础课学习/bfs_shortest_path_844.cpp
@@ -67,6 +67,71 @@ int main(){
 
 
 
+// 记录路径的写法
+
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+typedef pair<int, int> PII;
+
+const int N = 110;
+
+int n, m;
+int g[N][N], d[N][N];
+queue<PII> q ;
+PII before[N][N];
+
+int direc[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int bfs(){
+    while(!q.empty()){
+        auto t = q.front();
+        q.pop();
+        
+        for(int i = 0; i < 4; i++){
+            int x = t.first + direc[i][0];
+            int y = t.second + direc[i][1];
+            
+            if(x >= 0 && x < n && y >= 0 && y < m && g[x][y] == 0 && d[x][y] == -1){
+                d[x][y] = d[t.first][t.second] + 1;
+                q.push({x, y});
+                
+                before[x][y] = t; // 记录这一点由哪个点过来的
+            }
+        }
+    }
+    
+    int x = n - 1, y = m - 1;
+    while(x || y){
+        printf("%d %d\n", x, y);
+        auto t = before[x][y];
+        x = t.first;
+        y = t.second;
+    }
+    return d[n - 1][m - 1];
+}
+
+
+int main(){
+    scanf("%d%d", &n, &m);
+    
+    for(int i = 0; i < n; i ++)
+        for(int j = 0; j < m; j ++)
+            scanf("%d", &g[i][j]);
+    
+    memset(d, -1, sizeof(d));
+    
+    q.push({0,0});
+    d[0][0] = 0;
+    
+    printf("%d\n", bfs());
+    return 0;
+}
 
 
 
