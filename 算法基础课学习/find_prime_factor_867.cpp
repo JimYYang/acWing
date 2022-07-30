215d2e58bc59fb216a6e98bda4838bd0de07f22a
JimYYang <99114248+JimYYang@users.noreply.github.com>
2022-07-30 10:06:47 +0800

    Update find_prime_factor_867.cpp

--- 算法基础课学习/find_prime_factor_867.cpp
+++ 算法基础课学习/find_prime_factor_867.cpp
@@ -1,3 +1,6 @@
+// 由算术基本定理可知，任何一个大于1的正整数，都可以分解成若干个质数的乘积，并且这种乘积的形式是唯一的。
+
+
 #include <iostream>
 #include <algorithm>
 #include <cstring>
@@ -6,7 +9,7 @@ using namespace std;
 int n;
 
 void divide(int n){
-    for(int i = 2; i <= n / i; i ++){
+    for(int i = 2; i <= n / i; i ++){ // 这样看似找的是所有因数  但是其实是所有质因数  可以举例说明
         if(n % i == 0){
             int s = 0;
             while(n % i == 0){
@@ -17,7 +20,7 @@ void divide(int n){
         }
     }
     
-    if(n > 1)printf("%d %d\n", n, 1);
+    if(n > 1)printf("%d %d\n", n, 1); // 判断是否存在大于sqrt(n)的质因数
     puts("");
 }
 
