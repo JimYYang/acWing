849b07d6d093a18831e4ffe325eb361e78838302
jsyang <jsyang651@qq.com>
2022-09-02 14:44:33 +0800

    try2

--- try.cpp
+++ try.cpp
@@ -8,27 +8,35 @@ const int N = 3010;
 
 int dp[N][N];
 int a[N], b[N];
-int main(){
+int main()
+{
     int n;
     cin >> n;
-    
-    for(int i = 1; i <= n; i ++) cin >> a[i];
-    for(int i = 1; i <= n; i ++) cin >> b[i];
-    
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 1; i <= n; i++)
+        cin >> b[i];
+
     int maxl = 1;
-    for(int i = 1; i <= n; i ++){
+    for (int i = 1; i <= n; i++)
+    {
         maxl = 1;
-        for(int j = 1; j <= n; j ++){
+        for (int j = 1; j <= n; j++)
+        {
             dp[i][j] = dp[i - 1][j];
-            if(a[i] == b[j]) dp[i][j] = max(dp[i][j], maxl);
-            if(b[j] < a[i]) maxl = max(maxl, dp[i - 1][j] + 1);
+            if (a[i] == b[j])
+                dp[i][j] = max(dp[i][j], maxl);
+            if (b[j] < a[i])
+                maxl = max(maxl, dp[i - 1][j] + 1);
         }
     }
-    
+
     int res = 0;
-    for(int i = 1; i <= n; i ++) res = max(res, dp[n][i]);
-    
-    cout << res <<endl;
-    
+    for (int i = 1; i <= n; i++)
+        res = max(res, dp[n][i]);
+
+    cout << res << endl;
+    cout << endl;
     return 0;
 }
