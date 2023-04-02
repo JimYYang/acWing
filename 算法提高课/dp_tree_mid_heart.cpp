fd01477d42c144af060522cee288b992f4f7be8d
yjs <1729135661@qq.com>
2023-04-02 22:09:14 +0800

    dp_tree_mid_heart

--- 算法提高课/dp_tree_mid_heart.cpp
+++ 算法提高课/dp_tree_mid_heart.cpp
@@ -4,19 +4,24 @@
 
 using namespace std;
 
-const int N = 100010, M = 2 * N, INF = 0x3f3f3f3f;
+const int N = 10010, M = 2 * N, INF = 0x3f3f3f3f;
 int n;
 int h[N], e[M], ne[M], w[M], idx;
 int down1[N], down2[N], up[N];
 int son1[N], son2[N]; // 记录当前节点向下走的最大、次大距离是哪儿儿子更新过来的
-// 注意  次大值和最大值（即使相等）一定经过不同的儿子 因为最大距离的儿子的值就是最大值
+// 注意  次大值和最大值（即使相等）一定经过不同的儿子 因为最大距离的儿子的值就是最大值（son2其实不需要）
+bool is_leaf[N];
 int ans = INF;
-
 
 // 求出以u为根节点的子树向下走的最大距离
 // 这是用子节点更新父节点
 int dfs_down(int u, int father) {
+    // 这道题如果没有负权边可以在dfs_d时不用设置d1[u]和d2[u]为负无穷，
+    // 并且不用特判叶节点，因为d1和d2为零对于更新最大距离没有影响。 
+    // 但是如果有负权边就需要用设置为负无穷这种写法，并且也需要特判叶节点
+    // 对于本题 每个点都可以视为根节点
     down1[u] = down2[u] = -INF;
+    
     
     for (int i = h[u]; i != -1; i = ne[i]) {
         int j = e[i];
@@ -35,7 +40,8 @@ int dfs_down(int u, int father) {
             son2[u] = j;
         }
     }
-    if (down1[u] == -INF) down1[u] = down2[u] = 0;
+    if (down1[u] == -INF) 
+        down1[u] = down2[u] = 0, is_leaf[u] = true;
     return down1[u];
 }
 
@@ -74,7 +80,11 @@ int main() {
     
     dfs_up(1, -1);
     
-    for (int i = 1; i <= n; i ++) ans = min(ans, max(up[i], down1[i]));
+    ans = down1[1]; // 每个节点都可以当根 根只能向下走  边权为负数时需要这样考虑
+    for (int i = 2; i <= n; i ++) 
+        if (is_leaf[i]) ans = min(ans, up[i]);
+        else
+            ans = min(ans, max(up[i], down1[i]));
     printf("%d\n", ans);
     
     return 0;
